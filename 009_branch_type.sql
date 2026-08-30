cd452a6a486d7589e8854aca828594f6f477b28e
danchilia <danchilia16@gmail.com>
2026-08-31 00:42:48 +0300

    Fix 009 migration: use ADD COLUMN IF NOT EXISTS, remove programme table reference

--- 009_branch_type.sql
+++ 009_branch_type.sql
@@ -2,15 +2,5 @@
 -- Run on server: mysql -u cstuser -pCSTschool@2026! cst_schoolhub < 009_branch_type.sql
 
 ALTER TABLE `branch`
-    ADD COLUMN `branch_type` VARCHAR(20) NOT NULL DEFAULT 'school'
+    ADD COLUMN IF NOT EXISTS `branch_type` VARCHAR(20) NOT NULL DEFAULT 'school'
     AFTER `address`;
-
--- Mark branches that were seeded as universities (heuristic: they have programmes but no CBC learning areas)
-UPDATE `branch` b
-SET b.`branch_type` = 'university'
-WHERE EXISTS (
-    SELECT 1 FROM `programme` p WHERE p.branch_id = b.id LIMIT 1
-)
-AND NOT EXISTS (
-    SELECT 1 FROM `cbc_learning_areas` c WHERE c.branch_id = b.id LIMIT 1
-);
