e0e6f56bf3ffc0734d812315a20daea7cf5849b8
danchilia <danchilia16@gmail.com>
2026-08-31 00:43:41 +0300

    Fix 009 migration: plain ADD COLUMN for MySQL 5.x compatibility

--- 009_branch_type.sql
+++ 009_branch_type.sql
@@ -2,5 +2,5 @@
 -- Run on server: mysql -u cstuser -pCSTschool@2026! cst_schoolhub < 009_branch_type.sql
 
 ALTER TABLE `branch`
-    ADD COLUMN IF NOT EXISTS `branch_type` VARCHAR(20) NOT NULL DEFAULT 'school'
+    ADD COLUMN `branch_type` VARCHAR(20) NOT NULL DEFAULT 'school'
     AFTER `address`;
